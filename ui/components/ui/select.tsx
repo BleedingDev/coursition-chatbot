d1d2f8768bc6cb83ae659c35ee118799dd40b1dc
Petr Glaser <syreanis+1@gmail.com>
2025-08-31 21:10:49 +0200

    refactor: migrate to radix-ui common package

--- ui/components/ui/select.tsx
+++ ui/components/ui/select.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import * as SelectPrimitive from '@radix-ui/react-select';
+import { Select as SelectPrimitive } from 'radix-ui';
 import * as React from 'react';
 import { IoCheckmark, IoChevronDown, IoChevronUp } from 'react-icons/io5';
 
