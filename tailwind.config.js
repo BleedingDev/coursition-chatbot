2818133700a280534bc0ecf8f007e5b730755c37
Petr Glaser <syreanis+1@gmail.com>
2025-09-01 20:53:03 +0200

    refactor: finish migration to TW4

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mode: 'jit',
-  darkMode: 'class',
-  content: ['./index.html', './ui/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-};
